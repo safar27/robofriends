1d81f9726698adcfa3570cecc9a55687af2afa38
Suhani Jain <suhanijain123sj@gmail.com>
2022-04-04 01:03:00 +0530

    Updates

---
+++ precache-manifest.b4af86f8db8a3ce7abd56e34cd91a70b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b13e21f0b0c227c1c7f",
+    "url": "/robofriends/static/css/main.054a8fc8.chunk.css"
+  },
+  {
+    "revision": "2b13e21f0b0c227c1c7f",
+    "url": "/robofriends/static/js/main.2b13e21f.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/robofriends/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "11bebd3e60a167c55b15",
+    "url": "/robofriends/static/css/2.41486b5d.chunk.css"
+  },
+  {
+    "revision": "11bebd3e60a167c55b15",
+    "url": "/robofriends/static/js/2.11bebd3e.chunk.js"
+  },
+  {
+    "revision": "cf2212719657acf7eaba",
+    "url": "/robofriends/static/js/runtime~main.cf221271.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "181828827f4cb487646a17bf5af2dca4",
+    "url": "/robofriends/index.html"
+  }
+];
